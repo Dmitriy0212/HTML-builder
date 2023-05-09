95ce1d9a22589e6512859f9dd51beac19bb4af43
Dmitriy0212 <57304379+Dmitriy0212@users.noreply.github.com>
2023-05-09 19:28:18 +0300

    fix:changed the reading method to the one required by the assignment

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,7 +1,13 @@
-import { open } from 'node:fs/promises';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { createReadStream } from 'node:fs';
+import process from 'node:process';
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const stream = createReadStream(__dirname+'/text.txt');
 
-const file = await open('./01-read-file/text.txt');
+stream.on('readable', () => {
+    console.log(`${stream.read()}`);
+    process.exit(0)
+});
 
-for await (const line of file.readLines()) {
-  console.log(line);
-}
+
